7fd574245cf3e3c3337d4a3b5fd86dd14a6267ff
Anja Radenovic <Anja.Radenovic@endava.com>
2020-03-25 12:23:38 +0100

    change dependency injection to use annotations

--- src/main/java/com/endava/exercise/ExerciseApplication.java
+++ src/main/java/com/endava/exercise/ExerciseApplication.java
@@ -1,13 +1,15 @@
 package com.endava.exercise;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
 public class ExerciseApplication {
 
 	public static void main(String[] args) {
 
-		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ExerciseApplication.class);
+
 		Airplane airplane = context.getBean("airbus", Airplane.class);
 		System.out.println(airplane.toString());
 		System.out.println("Pilot's name is: " + airplane.getPilot().getName());
