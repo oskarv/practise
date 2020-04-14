a1075d24b476ac3a111c47a95d608ccf45f5e2a0
Anja Radenovic <anja.stanojevic@gmail.com>
2020-04-14 12:28:11 +0200

    reformat code

--- src/main/java/com/endava/algoritm/Solution.java
+++ src/main/java/com/endava/algoritm/Solution.java
@@ -2,7 +2,7 @@ package com.endava.algoritm;
 
 public class Solution {
 
-    public static int solution(int[] A){
+    public static int solution(int[] A) {
         int minMin = Integer.MAX_VALUE;
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
@@ -10,25 +10,24 @@ public class Solution {
         int maxMaxMax = Integer.MIN_VALUE;
 
         for (int i = 0; i < A.length; i++) {
-           if(maxMaxMax < A[i]  ){
-               max = maxMax;
-               maxMax = maxMaxMax;
-               maxMaxMax = A[i];
-           }else if(maxMax < A[i]){
-               max = maxMax;
-               maxMax = A[i];
-           }else if(max < A[i]){
-               max = A[i];
-           }
+            if (maxMaxMax < A[i]) {
+                max = maxMax;
+                maxMax = maxMaxMax;
+                maxMaxMax = A[i];
+            } else if (maxMax < A[i]) {
+                max = maxMax;
+                maxMax = A[i];
+            } else if (max < A[i]) {
+                max = A[i];
+            }
 
-           if(minMin > A[i]){
-               min = minMin;
-               minMin = A[i];
-           }else if(min > A[i]){
-               min = A[i];
-           }
+            if (minMin > A[i]) {
+                min = minMin;
+                minMin = A[i];
+            } else if (min > A[i]) {
+                min = A[i];
+            }
         }
-
         return Math.max(maxMaxMax * maxMax * max, maxMaxMax * minMin * min);
     }
 }
